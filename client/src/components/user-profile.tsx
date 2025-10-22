57069a1f0813bcef4b8ec4f897fbfa834c9f84d9
Rajnish Mehta <48842760-gandfitul@users.noreply.replit.com>
2025-10-22 16:39:35 +0000

    Restored to '700a0bdc02d1d708e5a2aaed1056dd37c08ed5b9'
    Replit-Restored-To: 700a0bdc02d1d708e5a2aaed1056dd37c08ed5b9

--- client/src/components/user-profile.tsx
+++ client/src/components/user-profile.tsx
@@ -25,12 +25,9 @@ function UserProfilePopup({ loginData, onLogout }: UserProfilePopupProps) {
   };
 
   const confirmLogout = () => {
+    onLogout();
     setShowLogoutConfirm(false);
     setIsOpen(false);
-    // Give UI time to update before logout
-    setTimeout(() => {
-      onLogout();
-    }, 100);
   };
 
   const cancelLogout = () => {
